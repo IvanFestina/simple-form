20feff77f2aad992c4b5d7a34f788c1b1b2b9da6
IvanFestina <ivanfestina@gmail.com>
2022-10-13 23:58:21 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { HashRouter } from 'react-router-dom';
+
+import s from './App.module.scss';
+import { SNACK_BAR_TYPES, SnackBar } from './common/SnackBar/SnackBar';
+import { Header } from './components/header/Header';
+import { RoutesApp } from './components/routes/RoutesApp';
+import { useAppSelector } from './utils/hooks';
+
+export const App = () => {
+  const appMessage = useAppSelector(state => state.products.appMessage);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.appBlock}>
+      <HashRouter>
+        <Header />
+        <div className={s.content}>
+          <RoutesApp />
+        </div>
+      </HashRouter>
+      {appMessage && <SnackBar message={appMessage} type={SNACK_BAR_TYPES.SUCCESS} />}
     </div>
   );
-}
-
-export default App;
+};
